5483ca82b44f03038c9d522e5621d4c949032a5e
Red Magnos <redmagnos@gmail.com>
2026-03-20 17:04:46 -0300

    Increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="sparkqlalchemy",
-    version="0.3",
+    version="0.3.1",
     package_dir={"": "src"},
     packages=find_namespace_packages(where="src"),
     install_requires=[
--- src/sparkqlalchemy/__init__.py
+++ src/sparkqlalchemy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3"
+__version__ = "0.3.1"
 
 from . import functions
 from .column import Column, WhenExpr

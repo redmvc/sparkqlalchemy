391e0fd9afbb3a677e3b48d1efd9ee35b8170ebb
Red Magnos <redmagnos@gmail.com>
2026-03-20 13:29:59 -0300

    Increase required version of typing_extensions

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="sparkqlalchemy",
-    version="0.2.1",
+    version="0.2.2",
     package_dir={"": "src"},
     packages=find_namespace_packages(where="src"),
     install_requires=[
         "setuptools",
         "pandas",
         "sqlalchemy",
-        "typing_extensions",
+        "typing_extensions>=4.15",
     ],
 )
